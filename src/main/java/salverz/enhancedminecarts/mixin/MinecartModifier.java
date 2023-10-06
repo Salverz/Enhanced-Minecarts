ac8e0dd8a102689157ba7060a4fc2de385ce42b9
Salverz <davidorona2@gmail.com>
2023-10-06 00:29:18 -0700

    Increased minecart max speed

--- src/main/java/salverz/enhancedminecarts/mixin/MinecartModifier.java
+++ src/main/java/salverz/enhancedminecarts/mixin/MinecartModifier.java
@@ -1,15 +1,15 @@
 package salverz.enhancedminecarts.mixin;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
-import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractMinecartEntity.class)
-abstract class MinecartModifier {
-    @Redirect(method = "getMaxSpeed",
-            at = @At(value = "INVOKE", target = ))
-    Minecart
+public abstract class MinecartModifier {
+    @Inject(method = "getMaxSpeed", at = @At("RETURN"), cancellable = true)
+    public void getMaxSpeed(CallbackInfoReturnable<Double> cir) {
+        cir.setReturnValue(20.0);
+    }
 }
